c15439c711de42b29b66444b8f959610e8a95954
CentKenneth <mycentkennethperia@gmail.com>
2025-10-04 16:30:46 +0800

    dfajghjadkhgjkahjkghajk

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -7,7 +7,7 @@ import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
-        title: 'Dashboard',
+        title: 'sdfsdfs',
         href: dashboard().url,
     },
 ];
